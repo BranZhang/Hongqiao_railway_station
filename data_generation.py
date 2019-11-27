8910815f24ff823fc212ff3fc0abdf576160ae64
zhangchi <zcqzcq@outlook.com>
2019-11-27 17:48:20 +0800

    完善铁路基础数据，完善车次爬虫。

--- data_generation.py
+++ data_generation.py
@@ -1,14 +1,36 @@
+import json
+import re
+from functools import reduce
 from multiprocessing import Pool
 from multiprocessing.dummy import Pool as ThreadPool
-from functools import reduce
 
 import requests
-import json
 
 STATION_NAME = "上海虹桥"
 START_TRAIN_INFO_URL = "https://g.xiuxiu365.cn/train_api/screen/msg/startTrainInfo2?station="
 END_TRAIN_INFO_URL = "https://g.xiuxiu365.cn/train_api/screen/msg/endTrainInfo2?station="
 TRAIN_INFO_URL = "https://g.xiuxiu365.cn/train_api/api/station/info?trainname="
+
+
+def string2time(timeString, minChange=0):
+    hourStr,minStr = timeString.split(":")
+    hour = int(hourStr)
+    min = int(minStr) + minChange
+    if min < 0:
+        hour -= 1
+        min += 60
+    if min >= 60:
+        hour += 1
+        min -= 60
+    return [hour, min]
+
+
+def stationString2Number(stationString):
+    match = re.search(r"\d+", stationString)
+    if match:
+        return int(match.group())
+    else:
+        return -1
 
 
 def get_train_info_by_name(name):
@@ -28,9 +50,9 @@ def get_train_info_by_name(name):
                 "route_type": "first",
                 "current_station": result_json['stationList'][i]['station'],
                 "next_station": result_json['stationList'][i+1]['station'],
-                "reach_hongqiao_time": result_json['stationList'][i+1]['dTime']-3,
-                "leave_hongqiao_time": result_json['stationList'][i+1]['dTime'],
-                "reach_hongqiao_station_number": result_json['stationList'][i+1]['exitStation'],
+                "arrive_hongqiao_time": string2time(result_json['stationList'][i+1]['dTime'],-3),
+                "departure_hongqiao_time": string2time(result_json['stationList'][i+1]['dTime']),
+                "arrive_hongqiao_station_number": stationString2Number(result_json['stationList'][i+1]['exitStation']),
                 "route_start_station": result_json['trainInfo']['startstation'],
                 "route_end_station": result_json['trainInfo']['endstation'],
                 "train_type": result_json['trainInfo']['traintype'],
@@ -41,9 +63,9 @@ def get_train_info_by_name(name):
                 "route_type": "last",
                 "current_station": result_json['stationList'][i-1]['station'],
                 "next_station": result_json['stationList'][i]['station'],
-                "reach_hongqiao_time": result_json['stationList'][i]['aTime'],
-                "leave_hongqiao_time": result_json['stationList'][i]['aTime']+3,
-                "reach_hongqiao_station_number": result_json['stationList'][i]['exitStation'],
+                "arrive_hongqiao_time": string2time(result_json['stationList'][i]['aTime']),
+                "departure_hongqiao_time": string2time(result_json['stationList'][i]['aTime'],3),
+                "arrive_hongqiao_station_number": stationString2Number(result_json['stationList'][i]['exitStation']),
                 "park_hongqiao_time": "",
                 "route_start_station": result_json['trainInfo']['startstation'],
                 "route_end_station": result_json['trainInfo']['endstation'],
@@ -55,9 +77,9 @@ def get_train_info_by_name(name):
                 "route_type": "pass_in",
                 "current_station": result_json['stationList'][i]['station'],
                 "next_station": result_json['stationList'][i+1]['station'],
-                "reach_hongqiao_time": result_json['stationList'][i]['aTime'],
-                "leave_hongqiao_time": result_json['stationList'][i]['dTime'],
-                "reach_hongqiao_station_number": result_json['stationList'][i]['exitStation'],
+                "arrive_hongqiao_time": string2time(result_json['stationList'][i]['aTime']),
+                "departure_hongqiao_time": string2time(result_json['stationList'][i]['dTime']),
+                "arrive_hongqiao_station_number": stationString2Number(result_json['stationList'][i]['exitStation']),
                 "route_start_station": result_json['trainInfo']['startstation'],
                 "route_end_station": result_json['trainInfo']['endstation'],
                 "train_type": result_json['trainInfo']['traintype'],
@@ -67,14 +89,14 @@ def get_train_info_by_name(name):
                 "route_type": "pass_out",
                 "current_station": result_json['stationList'][i-1]['station'],
                 "next_station": result_json['stationList'][i]['station'],
-                "reach_hongqiao_time": result_json['stationList'][i]['aTime'],
-                "leave_hongqiao_time": result_json['stationList'][i]['dTime'],
-                "reach_hongqiao_station_number": result_json['stationList'][i+1]['exitStation'],
+                "arrive_hongqiao_time": string2time(result_json['stationList'][i]['aTime']),
+                "departure_hongqiao_time": string2time(result_json['stationList'][i]['dTime']),
+                "arrive_hongqiao_station_number": stationString2Number(result_json['stationList'][i+1]['exitStation']),
                 "route_start_station": result_json['trainInfo']['startstation'],
                 "route_end_station": result_json['trainInfo']['endstation'],
                 "train_type": result_json['trainInfo']['traintype'],
             })
-    return []
+    return results
 
 
 def get_all_trains_info():
@@ -102,7 +124,9 @@ def get_all_trains_info():
     pool = ThreadPool(1)
     results = pool.map(get_train_info_by_name, list(trains))
 
-    return reduce(lambda x,y:x+y, results)
+    trains_detail = reduce(lambda x,y:x+y, results)
+    trains_detail.sort(key = lambda t:t['arrive_hongqiao_time'])
+    return trains_detail
 
 
 def build_rail_data():
@@ -114,5 +138,27 @@ def save2local():
 
 
 if __name__ == "__main__":
-    get_all_trains_info()
-
+    trains = get_all_trains_info()
+    trains = filter(lambda i:i['arrive_hongqiao_station_number']!=-1, trains)
+    # '*****':1
+    # '上海':1
+    # '上海南':4
+    # '上海西':1
+    # '南京南':6
+    # '嘉兴南':51
+    # '嘉善南':13
+    # '无锡东':7
+    # '昆山南':45
+    # '杭州东':9
+    # '松江南':13
+    # '桐乡':2
+    # '苏州':5
+    # '苏州北':32
+    # '金山北':7
+    # '阳澄湖':1
+    # __len__:16
+    for t in filter(lambda i:i['arrive_hongqiao_station_number']!=-1, trains):
+        print(
+            t['arrive_hongqiao_station_number'], "\t", 
+            t['next_station'] if t['route_type'] in ['pass_in', 'first'] else t['current_station'], "\t\t", 
+            t['arrive_hongqiao_time'])
